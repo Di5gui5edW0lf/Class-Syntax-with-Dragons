f82bd7e5e4122fbfc90f46479d7456cbf4c135f6
Angelica LaFlame <avguerrero90@gmail.com>
2022-10-18 16:04:52 -0700

    almost done

--- all-dragons.js
+++ all-dragons.js
@@ -1,5 +1,13 @@
-// Your code here
+const FriendlyDragon = require("./classes/friendly-dragon")
 
+const EvilDragon = require("./classes/evil-dragon")
+
+const falkor = new FriendlyDragon("Falkor"){
+  name: "Falkor";
+  color: "white";
+  lifeGoals: "save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster";
+  friend: "Bastian"
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
