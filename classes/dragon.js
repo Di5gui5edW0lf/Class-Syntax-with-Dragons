1afd948657b034db96610603e7dc5a1d76e63042
Di5gui5edW0lf <spearson@thedckc.com>
2022-10-18 14:27:03 -0700

    gitignore file

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,18 @@
-// Your code here
+const { returns } = require("chai-spies");
+
+class Dragon {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+
+breathesFire() {
+  returns `${this.name} breathes fire everywhere! Burn!!!.`
+}
+
+static cd
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
