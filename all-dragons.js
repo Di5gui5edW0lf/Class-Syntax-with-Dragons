7132d89a8b027f8a4190a1d09eec95cdf8f3cff6
Di5gui5edW0lf <spearson@thedckc.com>
2022-10-18 17:14:28 -0700

    Dragon's long problem solution all test specs passing

--- all-dragons.js
+++ all-dragons.js
@@ -1,13 +1,23 @@
 const FriendlyDragon = require("./classes/friendly-dragon")
-
 const EvilDragon = require("./classes/evil-dragon")
+const Dragon = require("./classes/dragon")
 
-const falkor = new FriendlyDragon("Falkor"){
-  name: "Falkor";
-  color: "white";
-  lifeGoals: "save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster";
-  friend: "Bastian"
-}
+const falkor = new FriendlyDragon(
+  "Falkor",
+  "white",
+  ["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"],
+  "Bastian"
+)
+const smaug = new EvilDragon(
+  "Smaug",
+  'black',
+  ["take over your mountain kingdom", "steal all your dwarven gold", "burn down your floating village"],
+  "Dwarf King"
+)
+
+const allDragons = Dragon.getDragons(falkor, smaug)
+
+module.exports.allDragons = allDragons
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
